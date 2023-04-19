decac6a8dc104b45b9f6e5d42c2a9522fc94b12c
William Lavery <lavery_william@yahoo.com>
2023-04-19 16:15:14 -0400

    Initial commit

---
+++ mild.js
@@ -0,0 +1,11 @@
+/* Write a JavaScript program that defines a variable that stores a Number. 
+The program should print out the String "even" if the Number is even, and the String "odd" 
+if the Number is odd. Hint: You may need to do some extra research on the remainder operator (%) 
+to solve this challenge. */
+
+var number = 3;
+if (number % 2 === 0) {
+    console.log("even");
+} else {
+    console.log("odd");
+}
